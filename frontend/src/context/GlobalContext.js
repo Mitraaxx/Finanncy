de79ff5d2a9b700355c520bbc8f3e0b92b789e47
Priyanshu Mitra <priyanshumitra986@gmail.com>
2025-06-05 21:16:31 +0530

    done

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from "react";
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://finanncy.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext()
 
@@ -238,3 +238,7 @@ export const GlobalProvider = ({children}) =>{
 export const useGlobalContext= () =>{
     return useContext(GlobalContext)
 }
+
+
+
+// http://localhost:5000

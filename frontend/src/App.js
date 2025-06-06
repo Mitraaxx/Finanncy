6d50bd6847cad9038cf3dc8d827fee5131e025c2
Priyanshu Mitra <143019799+Mitraaxx@users.noreply.github.com>
2025-06-06 19:07:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,6 +59,28 @@ function App() {
     setIsAuthenticated(token ? true : false);
   }, []);
 
+  // This used for the bot 
+  React.useEffect(() => {
+  const script1 = document.createElement("script");
+  script1.innerHTML = `
+    window.chtlConfig = { chatbotId: "5128478583" };
+  `;
+  document.body.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.src = "https://chatling.ai/js/embed.js";
+  script2.async = true;
+  script2.setAttribute("data-id", "5128478583");
+  script2.setAttribute("id", "chtl-script");
+  script2.type = "text/javascript";
+  document.body.appendChild(script2);
+
+  return () => {
+    document.body.removeChild(script1);
+    document.body.removeChild(script2);
+  };
+}, []);
+
   return (
     <Router>
       <AppStyled bg={bg} className="App">

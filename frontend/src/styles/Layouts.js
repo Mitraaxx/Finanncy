e8fd3168d26bb8c9ea9385dbc833981a914adefa
Priyanshu Mitra <143019799+Mitraaxx@users.noreply.github.com>
2025-06-06 14:31:36 +0530

    Update Layouts.js

--- frontend/src/styles/Layouts.js
+++ frontend/src/styles/Layouts.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const MainLayout = styled.div`
-    padding: 1rem; /* Reduced from 2rem */
+    padding: 2rem;
     height: 100%;
     display: flex;
-    gap: 1rem; /* Reduced from 2rem */
+    gap: 2rem;
 `;
 
 export const InnerLayout = styled.div`
-    padding: 1rem 0.75rem; /* Reduced from 2rem 1.5rem */
+    padding: 2rem 1.5rem;
     width: 100%;
+    zoom: 0.69;
+    position: fixed;
 `;

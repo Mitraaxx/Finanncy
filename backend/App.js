879eca2d6a04b71ce2f6ab2bb488b7f599ce9059
Priyanshu Mitra <priyanshumitra986@gmail.com>
2025-08-17 13:45:32 +0530

    ping

--- backend/App.js
+++ backend/App.js
@@ -16,9 +16,9 @@ app.use(cors())
 // routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+    res.status(200).send("Server is up and running!");
+});
 
 const server = () => {
     db()
